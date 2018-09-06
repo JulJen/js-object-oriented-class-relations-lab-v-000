5275777b712ccf659e6bd3b751ded4d7c6a5c9b2
Jul Jen <juliajen.prog@gmail.com>
2018-09-06 21:54:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,78 +1,59 @@
 // A driver has many trips, and has many passengers through trips.
-//
 // new Driver() - initialized with a name; returns a JavaScript object that has attributes of id, and name
 // trips() - returns all of the trips that a driver has taken
 // passengers() - returns all of the passengers that a driver has taken on a trip
 
 let store = { drivers: [], passengers: [], trips: [] }
 
+
 let driverId = 0
 
 class Driver {
   constructor(name) {
-    this.id = ++driverId
-    this.name = name
+    this.id = ++driverId;
+    this.name = name;
+
     store.drivers.push(this)
   }
-  passengers() {
-    return store.passengers.filter(trip => {
-      return trip.passengerId === this.id
-    })
-  }
+
   trips() {
     return store.trips.filter(trip => {
-      return trip.driverId === this.id
+      return trip.driverId === this.id;
+    })
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
     })
   }
 }
 
 
+// A passenger has many trips, and has many drivers through trips.
+// new Passenger() - initialized with a name; returns a JavaScript object that has attributes of id, and name
+// trips() - returns all of the trips that a passenger has taken
+// drivers() - returns all of the drivers that has taken a passenger on a trip
 
 let passengerId = 0
 
 class Passenger {
   constructor(name) {
-    this.id = ++passengerId
-    this.name = name
+    this.id = ++passengerId;
+    this.name = name;
+
     store.passengers.push(this)
   }
-  driver() {
-    return store.drivers.filter(function(driver) {
-      return driver.id === this.driverId
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id;
     })
   }
-}
 
-
-
-let tripId = 0
-
-class Trip {
-  constructor(driver, passenger) {
-    this.id = ++tripId
-    this.name = name
-    if(driver) {
-      this.driverId = driver.id
-    }
-    if(passenger) {
-      this.passengerId = passenger.id
-    }
-    store.trips.push(this)
-  }
-  setDriver(driver){
-    this.driverId = driver.id
-  }
-  setPassenger(passenger){
-    this.passengerId = passengerId.id
-  }
-  driver() {
-    return store.drivers.find(function(driver) {
-      return driver.id === this.driverId
-    })
-  }
-  passenger() {
-    return store.passengers.find(function(passenger) {
-      return passenger.id === this.passengerId
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
     })
   }
 }
@@ -82,3 +63,38 @@ class Trip {
 // new Trip() - initialized with an instance of driver and an instance of passenger; returns a JavaScript that has attributes id, driverId, and passengerId
 // driver() - returns the driver associated with the trip
 // passenger() - returns the passenger associated with the trip
+
+
+let tripId = 0
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+
+    if(driver) {
+      this.driverId = driver.id;
+    }
+    if(passenger) {
+      this.passengerId = passenger.id;
+    }
+
+    store.trips.push(this)
+  }
+
+  setDriver(driver){
+   this.driverId = driver.id
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  setPassenger(passenger){
+   this.passengerId = passenger.id
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
