7dd86b89ed945812ba25f2da904c071562430109
Jul Jen <juliajen.prog@gmail.com>
2018-09-06 21:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // A driver has many trips, and has many passengers through trips.
-//
 // new Driver() - initialized with a name; returns a JavaScript object that has attributes of id, and name
 // trips() - returns all of the trips that a driver has taken
 // passengers() - returns all of the passengers that a driver has taken on a trip
@@ -30,6 +29,11 @@ class Driver {
 }
 
 
+// A passenger has many trips, and has many drivers through trips.
+// new Passenger() - initialized with a name; returns a JavaScript object that has attributes of id, and name
+// trips() - returns all of the trips that a passenger has taken
+// drivers() - returns all of the drivers that has taken a passenger on a trip
+
 let passengerId = 0
 
 class Passenger {
@@ -51,6 +55,12 @@ class Passenger {
     })
   }
 }
+
+
+// A trip belongs to a driver and belongs to a passenger.
+// new Trip() - initialized with an instance of driver and an instance of passenger; returns a JavaScript that has attributes id, driverId, and passengerId
+// driver() - returns the driver associated with the trip
+// passenger() - returns the passenger associated with the trip
 
 
 let tripId = 0
@@ -80,9 +90,3 @@ class Trip {
     })
   }
 }
-
-
-// A trip belongs to a driver and belongs to a passenger.
-// new Trip() - initialized with an instance of driver and an instance of passenger; returns a JavaScript that has attributes id, driverId, and passengerId
-// driver() - returns the driver associated with the trip
-// passenger() - returns the passenger associated with the trip
