746fca6c1065125aa29729446b0aad5513a36c9c
Jul Jen <juliajen.prog@gmail.com>
2018-09-06 21:30:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,22 +58,16 @@ class Trip {
     }
     store.trips.push(this)
   }
-  // setDriver(driver){
-  //   this.driverId = driver.id
-  // }
-  // setPassenger(passenger){
-  //   this.passengerId = passengerId.id
-  // }
-  // driver() {
-  //   return store.drivers.find(function(driver) {
-  //     return driver.id === this.driverId
-  //   })
-  // }
-  // passenger() {
-  //   return store.passengers.find(function(passenger) {
-  //     return passenger.id === this.passengerId
-  //   })
-  // }
+  driver() {
+    return store.drivers.find(function(driver) {
+      return driver.id === this.driverId
+    })
+  }
+  passenger() {
+    return store.passengers.find(function(passenger) {
+      return passenger.id === this.passengerId
+    })
+  }
 }
 
 
