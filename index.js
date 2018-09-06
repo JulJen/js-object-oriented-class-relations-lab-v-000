9e6a95f10c6aa35069eff31ad5be691d9a61ff35
Jul Jen <juliajen.prog@gmail.com>
2018-09-06 21:38:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@
 
 let store = { drivers: [], passengers: [], trips: [] }
 
-
 let driverId = 0
+let passengerId = 0
+let tripId = 0
+
 
 class Driver {
   constructor(name) {
@@ -30,8 +32,6 @@ class Driver {
 }
 
 
-let passengerId = 0
-
 class Passenger {
   constructor(name) {
     this.id = ++passengerId;
@@ -53,8 +53,6 @@ class Passenger {
 }
 
 
-let tripId = 0
-
 class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId;
@@ -67,13 +65,13 @@ class Trip {
     }
     store.trips.push(this)
   }
-  
+
   driver() {
     return store.drivers.find(function(driver) {
       return driver.id === this.driverId;
     })
   }
-  
+
   passenger() {
     return store.passengers.find(function(passenger) {
       return passenger.id === this.passengerId;
